f93443802db98380caa143009d6749feb4bb4ae0
Plinio Grijalba <plinio.grijalba@davincitech.co>
2021-05-10 09:49:12 -0500

    genereate services

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,16 @@ import { OrdersController } from './orders/orders.controller';
 import { UsersController } from './users/users.controller';
 import { CustomersController } from './customers/customers.controller';
 import { BrandsController } from './brands/brands.controller';
+import { ProductsService } from './products/products.service';
+import { BrandsService } from './brands/brands.service';
+import { CategoriesService } from './categories/categories.service';
+import { CostumersService } from './costumers/costumers.service';
+import { OrdersService } from './orders/orders.service';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ProductsController, CategoriesController, OrdersController, UsersController, CustomersController, BrandsController],
-  providers: [AppService],
+  providers: [AppService, ProductsService, BrandsService, CategoriesService, CostumersService, OrdersService, UsersService],
 })
 export class AppModule {}
