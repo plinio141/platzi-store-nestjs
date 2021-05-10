593a3f0e0ab61de447a7d0b996176ad040b05cd3
Plinio Grijalba <plinio.grijalba@davincitech.co>
2021-05-09 20:59:16 -0500

    generate controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './products/products.controller';
+import { CategoriesController } from './categories/categories.controller';
+import { OrdersController } from './orders/orders.controller';
+import { UsersController } from './users/users.controller';
+import { CustomersController } from './customers/customers.controller';
+import { BrandsController } from './brands/brands.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductsController, CategoriesController, OrdersController, UsersController, CustomersController, BrandsController],
   providers: [AppService],
 })
 export class AppModule {}
